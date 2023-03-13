2c9edc1c518cc93addf4d9a8b23bb18f25237615
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2023-03-13 11:30:53 +0000

    final commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -9,5 +9,5 @@ blogRoutes.post('/blog',blogController.addNewBlog)
 blogRoutes.get('/view',blogController.viewAllBlogs)
 blogRoutes.get('/view/:id', blogController.viewBlog)
 blogRoutes.put('/view/:id',blogController.editBlog)
-blogRoutes.post('/delete/:id',blogController.deleteBlog )
+blogRoutes.delete('/delete/:id',blogController.deleteBlog )
 module.exports = blogRoutes;
